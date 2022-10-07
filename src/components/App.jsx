74da8d5413f42dffd9602176833865a3421b8c10
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-10-07 22:38:57 +0200

    change App to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
-import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
+import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { getPhotos } from 'services/api';
 import { Searchbar } from './Searchbar/Searchbar';
@@ -10,117 +9,92 @@ import { Modal } from './Modal/Modal';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Text } from './Text';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    items: [],
-    loading: false,
-    isVisible: false,
-    error: null,
-    modalOpen: false,
-    modalContent: '',
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [error, setError] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if ((query && prevState.query !== query) || page > prevState.page) {
-      this.fetchImages(query, page);
+  useEffect(() => {
+    const fetchImages = async () => {
+      setLoading(true);
+
+      try {
+        const data = await getPhotos(query, page);
+        setItems(prevItems => {
+          return [...prevItems, ...data.hits];
+        });
+
+        if (page < Math.ceil(data.total / 12)) {
+          setIsVisible(true);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (query) {
+      fetchImages();
     }
-  }
+  }, [query, page]);
 
-  async fetchImages() {
-    const { query, page } = this.state;
-    this.setState({
-      loading: true,
-    });
-
-    try {
-      const data = await getPhotos(query, page);
-      this.setState(({ items }) => {
-        return {
-          items: [...items, ...data.hits],
-          isVisible: page < Math.ceil(data.total / 12),
-        };
-      });
-    } catch (error) {
-      this.setState({
-        error,
-      });
-    } finally {
-      this.setState({
-        loading: false,
-      });
-    }
-  }
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { value, name } = e.target.elements.query;
+    const { value } = e.target.elements.query;
     if (value.trim() === '') {
-      toast.warning('Enter a search request', {
+      toast.error('Enter a search request', {
         theme: 'colored',
-        autoClose: 3000,
+        autoClose: 2500,
       });
       return;
     }
-    if (value.length !== 0 && value !== this.state.query) {
-      this.setState({
-        [name]: value,
-        items: [],
-        page: 1,
-      });
+    if (value.length !== 0 && value !== query) {
+      setQuery(value);
+      setItems([]);
+      setPage(1);
     }
   };
 
-  loadMore = () => {
-    this.setState(({ page }) => {
-      return {
-        page: page + 1,
-      };
-    });
+  const loadMore = () => {
+    setPage(page => page + 1);
   };
 
-  openModal = e => {
+  const openModal = e => {
     const modalContent = e.target.dataset.large;
-    this.setState({
-      modalOpen: true,
-      modalContent: modalContent,
-    });
+    setModalOpen(true);
+    setModalContent(modalContent);
   };
 
-  closeModal = () => {
-    this.setState({
-      modalOpen: false,
-      modalContent: '',
-    });
+  const closeModal = () => {
+    setModalOpen(false);
+    setModalContent('');
   };
 
-  render() {
-    const { items, loading, error, modalOpen, modalContent, isVisible } =
-      this.state;
-    const { handleSubmit, loadMore, openModal, closeModal } = this;
-    const isPhotos = Boolean(items.length > 0);
+  const isPhotos = Boolean(items.length > 0);
 
-    return (
-      <>
-        <Searchbar onSubmit={handleSubmit} />
-        {loading && <Loader />}
-        {error && <Text>Something went wrong...</Text>}
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      {loading && <Loader />}
+      {error && <Text>Something went wrong...</Text>}
 
-        {isPhotos ? (
-          <ImageGallery items={items} onClick={openModal} />
-        ) : (
-          <Text>Sorry, there are no images</Text>
-        )}
-        {isVisible && !loading && <Button onClick={loadMore} />}
-        {modalOpen && (
-          <Modal onClose={closeModal} content={modalContent}>
-            <img src={modalContent} alt="" />
-          </Modal>
-        )}
-        <ToastContainer />
-      </>
-    );
-  }
-}
+      {isPhotos ? (
+        <ImageGallery items={items} onClick={openModal} />
+      ) : (
+        <Text>Sorry, there are no images</Text>
+      )}
+      {isVisible && !loading && <Button onClick={loadMore} />}
+      {modalOpen && (
+        <Modal onClose={closeModal} content={modalContent}>
+          <img src={modalContent} alt="" />
+        </Modal>
+      )}
+      <ToastContainer />
+    </>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,3 @@
-// import { Component } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { Header, Form, Input, SearchButton } from './Searchbar.styled';
@@ -24,64 +23,3 @@ export const Searchbar = ({ onSubmit }) => {
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
 };
-
-// export class Searchbar extends Component {
-//   static propTypes = {
-//     onSubmit: PropTypes.func.isRequired,
-//   };
-
-//   state = {
-//     query: '',
-//   };
-
-//   handleChange = e => {
-//     this.setState({ query: e.target.value });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const { query } = this.state;
-//     const { onSubmit } = this.props;
-
-//     this.props.onSubmit({ ...this.state });
-//     this.setState({ query: '' });
-
-//     if (query.trim() === '') {
-//       toast.warning('Enter a search request', {
-//         position: toast.POSITION.TOP_RIGHT,
-//         theme: 'colored',
-//         autoClose: 3000,
-//       });
-//       return;
-//     }
-
-//     onSubmit({ ...this.state });
-//     this.setState({
-//       query: '',
-//     });
-//   };
-
-//   render() {
-//     const { query } = this.state;
-//     const { handleChange, handleSubmit } = this;
-
-//     return (
-//       <Header>
-//         <Form onSubmit={handleSubmit}>
-//           <SearchButton type="submit" onClick={handleSubmit}>
-//             <FaSearch size={14} />
-//           </SearchButton>
-
-//           <Input
-//             value={query}
-//             onChange={handleChange}
-//             type="text"
-//             autoFocus
-//             autoComplete="off"
-//             placeholder="Search images and photos"
-//           />
-//         </Form>
-//       </Header>
-//     );
-//   }
-// }
