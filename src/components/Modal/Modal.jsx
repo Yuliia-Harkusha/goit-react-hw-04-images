fdd45cf7bf4cde9a33ed409b8b205de00b76fcbd
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-10-07 23:08:29 +0200

    change modal to hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,38 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
+import propTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalWindow, CloseButton } from './Modal.styled';
 
 const modalRoot = document.getElementById('modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeModal);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeModal);
-  }
-
-  closeModal = ({ target, currentTarget, code }) => {
+export const Modal = ({ onClose, content, children }) => {
+  const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const { closeModal } = this;
-    const { children, content } = this.props;
-    return createPortal(
-      <Overlay onClick={closeModal}>
-        <ModalWindow>
-          <img src={content} alt="" loading="lazy" />
-          <CloseButton onClick={closeModal}>X</CloseButton>
-          {children}
-        </ModalWindow>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+    return () => {
+      document.removeEventListener('keydown', closeModal);
+    };
+  });
+
+  return createPortal(
+    <Overlay onClick={closeModal}>
+      <ModalWindow>
+        <img src={content} alt="" loading="lazy" />
+        <CloseButton onClick={closeModal}>X</CloseButton>
+        {children}
+      </ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+Modal.proptypes = {
+  onClose: propTypes.func,
+  content: propTypes.string,
+  children: propTypes.element,
+};
